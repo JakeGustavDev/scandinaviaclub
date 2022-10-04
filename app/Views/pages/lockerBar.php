ebfee20314c54e36f47fb4801b65372de1479b2a
BlackMeMyself73 <davidethefirst.dlp@gmail.com>
2022-10-04 18:20:02 +0200

    lockerBar

--- app/Views/pages/lockerBar.php
+++ app/Views/pages/lockerBar.php
@@ -1,9 +1,59 @@
 <?php echo view("templates/header"); ?>
-<main>
-    <div class="consummation">
 
+<main class="lockerBar">
+    <div class="consummation">
+        <div class="info">
+            <h4>Locker: </h4>
+            <h4>Ocupante:</h4>
+            <h4>Cover:</h4>
+        </div>
+        <table>
+            <thead>
+                <th class="concept">Concepto</th>
+                <th class="quantity">Cantidad</th>
+                <th class="quantity">Total</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="concept">Refresco</td>
+                    <td class="quantity">10</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
     <div class="proser">
-
+        <div>Masaje 1</div>
+        <div>Masaje 2</div>
+        <div>Dep. Ano y Glúteos</div>
+        <div>Dep. Axilas</div>
+        <div>Dep. Brazos</div>
+        <div>Dep. Espalda</div>
+        <div>Dep. Nariz</div>
+        <div>Dep. Pantorrilla o Muslo</div>
+        <div>Dep. Parte Íntima</div>
+        <div>Dep. Pecho y Abdomen</div>
+        <div>Dep. Piernas Completas</div>
+        <div>Facial Escandinavia</div>
+        <div>Facial Profundo</div>
+        <div>Toalla</div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
     </div>
 </main>
